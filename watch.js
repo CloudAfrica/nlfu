7a7b5a2681e5357246a57d976fb0cb75b5dc46ad
Len Weincier <len@cloudafrica.net>
2013-08-16 16:17:22 +0200

    watching a directory

--- watch.js
+++ watch.js
@@ -1,22 +1,36 @@
 var fs = require('fs');
 var send = require('./send_udp');
 
-var filename = process.argv[2];
+var dirname = process.argv[2];
 
-fs.watchFile(filename, function (curr, prev) {
-  console.log('the current size is: ' + curr.size);
-  console.log('the previous size was: ' + prev.size);
-  var stream = fs.createReadStream(filename, {autoClose: true, start: prev.size});
-  stream.addListener("data", function (lines) {
-    lines = lines.toString('utf-8');
-    lines = lines.split("\n");
-    for (var i = 0; i < lines.length; i++) {
-      if (lines[i].length > 0) {
-        console.log('[' + lines[i] + ']');
-        send(lines[i]);
-      }
+var files = [];
+
+fs.watch(dirname, function (event, filename) {
+  if (filename) {
+    if (files.indexOf(filename) == -1) {
+      files.push(filename);
+      monitorFile(filename);
     }
-  });
+  }
 });
+
+var monitorFile = function (filename) {
+  console.log('starting watch for ' + filename);
+  fs.watchFile(filename, function (curr, prev) {
+    console.log('the current size is: ' + curr.size);
+    console.log('the previous size was: ' + prev.size);
+    var stream = fs.createReadStream(filename, {autoClose: true, start: prev.size});
+    stream.addListener("data", function (lines) {
+      lines = lines.toString('utf-8');
+      lines = lines.split("\n");
+      for (var i = 0; i < lines.length; i++) {
+        if (lines[i].length > 0) {
+          console.log('[' + lines[i] + ']');
+          send(lines[i]);
+        }
+      }
+    });
+  });
+}
 
 
