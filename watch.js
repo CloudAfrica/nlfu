4c55d903c39093536508395b450ea30fb4062290
Len Weincier <len@cloudafrica.net>
2013-08-16 15:19:30 +0200

    watch and show changes

--- watch.js
+++ watch.js
@@ -1,16 +1,31 @@
 var fs = require('fs');
 
-fs.watch('/tmp', function (event, filename) {
-  console.log('event is: ' + event);
-  if (filename) {
-    console.log('filename provided: ' + filename);
-  } else {
-    console.log('filename not provided');
-  }
+//fs.watch('/tmp', function (event, filename) {
+//  console.log('event is: ' + event);
+//  if (filename) {
+//    console.log('filename provided: ' + filename);
+//  } else {
+//    console.log('filename not provided');
+//  }
+//});
+
+var filename = '/tmp/message.text';
+
+fs.watchFile(filename, function (curr, prev) {
+  console.log('the current size is: ' + curr.size);
+  console.log('the previous size was: ' + prev.size);
+
+  var stream = fs.createReadStream(filename, {autoClose: true, start: prev.size}); //, {start: start, end: stats.size});
+
+  stream.addListener("data", function (lines) {
+    lines = lines.toString('utf-8');
+    lines = lines.split("\n");
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i].length > 0) {
+        console.log('[' + lines[i] + ']');
+      }
+    }
+  });
 });
 
 
-fs.watchFile('/tmp/message.text', function (curr, prev) {
-  console.log('the current mtime is: ' + curr.mtime);
-  console.log('the previous mtime was: ' + prev.mtime);
-});
